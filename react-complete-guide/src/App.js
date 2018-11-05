cb34204fcec8be167450f2860084c8921f9730c7
Hamza Bhatti <hamza.bhatti@dunnhumby.com>
2018-11-05 17:27:55 +0000

    Added class state property to App.js. Also added onclick listener to button

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
+import person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hamza', age: 24 },
+      { name: 'Max', age: 28 },
+      { name: 'Bruno', age: 26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was Clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This isnt html</p>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>        
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>      
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?' ))
