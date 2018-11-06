dc192409f239be4b93772bca8d216f4b3052f87d
Hamza Bhatti <hamza.bhatti@dunnhumby.com>
2018-11-06 17:00:22 +0000

    Passing button click to person component. Passing argument to switch handle

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     otherState: 'Some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was Clicked!');
     // DONT DO THIS: this.state.persons[0].name = "Hamza Bhatti"
     this.setState({
       persons: [
-        { name: 'Hamza Bhatti', age: 24 },
+        { name: newName, age: 24 },
         { name: 'Max', age: 28 },
         { name: 'Bruno', age: 30 }
       ] 
@@ -28,10 +28,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>        
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Hamza!!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>        
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Hamza!')}>My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler.bind(this, 'Batman')}/>
         <hr/>
         <p>{this.state.otherState}</p>
 
