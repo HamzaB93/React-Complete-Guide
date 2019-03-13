4127276d77be2eca0f0e76a944f97295356c11f5
Hamza Bhatti <hamza.bhatti@dunnhumby.com>
2019-03-13 13:57:51 +0000

    Changed Person(s) to class based components and added lifecycle hooks for class based components

--- react-complete-guide/src/Containers/App.js
+++ react-complete-guide/src/Containers/App.js
@@ -27,6 +27,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount ');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
+  }
+
   // state = {
   //   persons: [
   //     { id: 'abdfsfd', name: 'Hamza', age: 24 },
